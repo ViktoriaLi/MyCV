a39f61799c200a76f12160d1e84ed69fe577dd98
ViktoriaLi <viktoria.likhotkina@gmail.com>
2019-05-02 16:53:49 +0300

    Add app icon

--- CV_by_V.Likhotkina/CV_by_V.Likhotkina/Controller/ShowSertificateViewController.swift
+++ CV_by_V.Likhotkina/CV_by_V.Likhotkina/Controller/ShowSertificateViewController.swift
@@ -9,23 +9,26 @@
 import UIKit
 
 class ShowSertificateViewController: UIViewController {
+    
     @IBOutlet weak var sertificateImageView: UIImageView!
+    @IBOutlet weak var certificateScrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        certificateScrollView.delegate = self
         sertificateImageView.image = UIImage(named: "unit_progress")
-        // Do any additional setup after loading the view.
+        /*sertificateImageView.sizeToFit()
+        certificateScrollView?.contentSize = sertificateImageView.frame.size
+        certificateScrollView.addSubview(sertificateImageView)
+        view.addSubview(certificateScrollView)*/
+        certificateScrollView.minimumZoomScale = 1.0
+        certificateScrollView.maximumZoomScale = 10.0
+        
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension ShowSertificateViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return sertificateImageView
     }
-    */
-
 }
